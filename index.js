c325eecee14ebdde99d09b1a10abfd802c853956
sanober15 <sanober@sanober.me>
2017-12-03 08:16:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  var spy = string.toUpperCase()
+  console.log(`${spy}`)
+}
+ 
+function logWhisper(string){
+  var spy = string.toLowerCase()
+  console.log(`${spy}`)                                                      
+}
